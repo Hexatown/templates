7d9d194b7b770786b88c0cdc9e57b47cb8d5b9e7
Niels Gregers Johansen <niels@timeboxer.dk>
2021-01-21 20:14:12 +0100

    added more

--- src/jobs/powershell/index.ps1
+++ src/jobs/powershell/index.ps1
@@ -1,1 +1,5 @@
-# placeholder
+. "$PSScriptRoot\.hexatown.com.ps1"                # Load the Hexatown framework
+$hexatown = Start-Hexatown  $myInvocation          # Start the framework
+
+
+Stop-Hexatown $hexatown                            # Stop the framework       

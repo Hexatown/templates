604762c3a09e43134455d7200f8c7f4723da18a8
Niels Gregers Johansen <niels@timeboxer.dk>
2021-01-21 21:22:37 +0100

    Extended "blank" template

--- src/jobs/powershell/index.ps1
+++ src/jobs/powershell/index.ps1
@@ -1,5 +1,4 @@
 . "$PSScriptRoot\.hexatown.com.ps1"                # Load the Hexatown framework
 $hexatown = Start-Hexatown  $myInvocation          # Start the framework
-
-
+Read $hexatown my self | fl                        # Read my self 
 Stop-Hexatown $hexatown                            # Stop the framework       
